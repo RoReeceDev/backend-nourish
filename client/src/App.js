ce3dd5bf9c28d50ae2e041e0c2549daaba3195e0
RoReeceDev <roreecedev@gmail.com>
2023-12-19 12:10:04 -0500

    new setup

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+            Hello Frontend. Hurry the hell up - Quite Frankly, BackendTeam (Hozay (mostly), Kyle, Finklenator, Ro) 
         </p>
         <a
           className="App-link"
